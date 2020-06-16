612738fb6f05cf389fd09c8f72495d420b6a06e6
MarcusLariJ <62110139+MarcusLariJ@users.noreply.github.com>
2020-06-16 10:46:25 +0200

    Update main.c

--- src/main.c
+++ src/main.c
@@ -8,21 +8,54 @@
 #include "lcd.h"
 #include "time.h"
 #include "spaceShip.h"
+#include "updateships.h"
 
+#define MAX_COUNT 200
+
+static int32_t checkCount;
+
+void TIM2_IRQHandler(void){
+
+    checkCount ++;
+    TIM2->SR &= ~0x0001; // Clear interrupt bit
+}
 
 int main(void)
 {
     uart_init(115200);
+
+    /*
     lcd_init();
 
     uint8_t *buffer[512];
     memset(buffer, 0x00, 512);
     lcd_face_six(buffer);
     lcd_push_buffer(buffer);
+    */
 
+    // initialize everything
 
+    gobj_t player;
+    gobj_t laser;
 
+    initPlayer(&player);
+    // Initialize timer
+    setupTimer();
+    startTimer();
 
- while(1) {}
+    while(1){
+
+        updatePlayer(&player, &laser);
+        updateLaser(&laser);
+
+        if (checkCount >= MAX_COUNT){
+            clrscr();
+            drawObj(&player);
+            //drawObj(&laser);
+
+            checkCount -= MAX_COUNT;
+        }
+    }
 }
+
 
